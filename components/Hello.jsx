b70e1b00367b0bd2fd0825ec5bf092bafaa58dd0
msclal <mika1shanela@gmail.com>
2023-06-22 17:19:00 -0700

    create hello container

---
+++ components/Hello.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import Image from "next/image";
+import { FiMail } from "react-icons/fi";
+import { AiFillGithub } from "react-icons/ai";
+import { ImLinkedin2 } from "react-icons/im";
+import ProfilePNG from "@/public/gallery/hello.png";
+import ProfileSVG from "@/public/gallery/hello.svg";
+
+const Hello = () => {
+  return (
+    <>
+      <div className="bg-gray-200 w-full rounded-[20px] flex p-10 max-md:hidden">
+        <div className="flex flex-wrap justify-between items-center">
+          <div className="space-y-14 w-[60%]">
+            <div className="space-y-4 break-before-all">
+              <p className="text-4xl font-semibold">Hello, {"I'm"} Mika!</p>
+              <div>
+                <p className="texl-lg lg:text-xl">
+                  Software Engineer based in Southern California. Trying to make
+                  it out the trenches. After hours, I eat {"Häagen-Dazs"} and
+                  watch the Golden State Warriors self-detonate their roster.
+                </p>
+                {/* <p className="text-xl">
+              After hours, I eat Haagen daaz and watch the Golden State Warriors
+              self-detonate their roster.
+            </p> */}
+              </div>
+              <p className="texl-lg lg:text-xl">
+                Hayley Williams from Paramore, top 5 woman ever.
+              </p>
+            </div>
+            <div className="flex gap-x-5 text-3xl">
+              <div className="w-fit p-3 bg-gray-100 rounded-[10px]">
+                <FiMail />
+              </div>
+              <div className="w-fit p-3 bg-gray-100 rounded-[10px]">
+                <AiFillGithub />
+              </div>
+              <div className="w-fit p-3 bg-gray-100 rounded-[10px]">
+                <ImLinkedin2 />
+              </div>
+            </div>
+          </div>
+          <Image src={ProfileSVG} alt="Mika Profile Picture" />
+        </div>
+      </div>
+      <div className="md:hidden bg-gray-200 w-full rounded-[20px] flex flex-col p-8 px-5 justify-center items-center gap-y-5">
+        <div className="flex flex-col items-center gap-y-1">
+          <Image
+            src={ProfilePNG}
+            alt="Mika Profile Picture"
+            className="max-sm:w-40"
+            //   width={40}
+            //   height={400}
+            //   quality={100}
+            // className="rounded-full"
+            //   className="min-w-1/4"
+          />
+          <p className="text-lg font-medium">Mika Shanela Carodan</p>
+          <p className="underline">{"+1 (909) 214-5009"}</p>
+        </div>
+        <div className="flex gap-x-5 text-3xl">
+          <div className="w-fit p-3 bg-gray-100 rounded-[10px]">
+            <FiMail />
+          </div>
+          <div className="w-fit p-3 bg-gray-100 rounded-[10px]">
+            <AiFillGithub />
+          </div>
+          <div className="w-fit p-3 bg-gray-100 rounded-[10px]">
+            <ImLinkedin2 />
+          </div>
+        </div>
+        <p className="text-center">
+          Software Engineer based in Southern California. Trying to make it out
+          the trenches. After hours, I eat {"Häagen-Dazs"} and watch the Golden
+          State Warriors self-detonate their roster.
+        </p>
+        {/* <p>Mika Shanela Carodan</p>
+        <p>Mika Shanela</p> */}
+      </div>
+    </>
+  );
+};
+
+export default Hello;
