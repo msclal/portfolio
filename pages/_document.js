7b33f3e71b13f4fdf4caabaa190d7c62384eda05
msclal <mika1shanela@gmail.com>
2023-06-25 22:56:21 -0700

    add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link rel="icon" href="/m.png" />
       <body>
         <Main />
         <NextScript />
