516e84b469cf7ec2517cd7f40be8428ff1660094
msclal <mika1shanela@gmail.com>
2023-08-19 19:01:48 -0700

    testing meta image

--- pages/_document.js
+++ pages/_document.js
@@ -17,13 +17,12 @@ export default function Document() {
         name="description"
         content="Home page for Mika Shanela's portfolio site!"
       />
-      <meta property="og:image" content="/gallery/hello.png" />
-      <meta name="viewport" content="width=device-width" />
+      <meta property="og:image" content="/assets/egg.png" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta charSet="utf-8" />
       <meta property="og:type" content="website" />
       <meta property="og:locale" content="en_IE" />
       <meta name="robots" content="index,follow" />
-      {/* </head> */}
       <link rel="icon" href="/assets/egg.png" />
 
       {/* <!-- Google tag (gtag.js) --> */}
